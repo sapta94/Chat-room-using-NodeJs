14329280426aefa2be69313ed208787b78859825
Saptarshi Dey <saptarshidey.2009@rediffmail.com>
2017-06-11 00:19:40 +0530

    Server file with socket configuration added with basic chat page

---
+++ index.js
@@ -0,0 +1,32 @@
+var express = require('express')
+var app = express()
+const path = require('path')
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
+var bodyParser=require('body-parser');
+app.use(express.static(__dirname+'/public'))
+var un;
+app.use(bodyParser.urlencoded({
+    extended:true
+}))
+
+app.use(bodyParser.json())
+
+app.post('/setname',function(req,res){
+    un=req.body.user;
+    console.log(un);
+})
+
+io.on('connection', function(socket){
+    console.log('A user connected '+socket.id);
+
+   socket.on('messagetoserver', function(msg){
+    console.log("Received message: "+msg);
+    io.emit('appendmessage', {msg:msg, id:socket.id,name: un});
+  });
+});
+
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});
